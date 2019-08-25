7891c4554b807b604bfc7c32aa3d19d70c43727a
Chiru B <chirub@foretag.co>
2019-08-25 21:47:43 +0100

    React based media manager

--- source/scripts/media.js
+++ source/scripts/media.js
@@ -1,15 +1,36 @@
 //@flow
 
 import * as React from 'react'
+
+
 export default class Media extends React.Component <IMediaOptions> {
 
+	element: { 
+		current?: HTMLAudioElement | HTMLVideoElement | null
+	}  | null
+
 	constructor(props: IMediaOptions) {
-		super(props)		
+		super(props)
+
+		this.element = React.createRef()
+	}
+
+	play() {
+		if (this.element && this.element.current) {
+			this.element.current.play()
+		}
+	}
+
+	pause() {
+		if (this.element && this.element.current) {
+			this.element.current.pause()
+		}
 	}
 
 	render(): React.Node {
 
-		if(this.props.type === 'video') {
+		if (this.props.type === 'video') {
+
 			return(
 				<video
 					autoplay={this.props.preload}
@@ -18,11 +39,14 @@ export default class Media extends React.Component <IMediaOptions> {
 					controls={this.props.controls}
 					loop={this.props.loop}
 					muted={this.props.muted}
+					ref={video => (this.element = video)}
 				>
 					<source src={this.props.source} type='video/mp4' />
 				</video>
 			)
-		} else {
+
+		} else if (this.props.type === 'audio') {
+
 			return(
 				<audio
 					autoplay={this.props.autoplay}
@@ -30,10 +54,16 @@ export default class Media extends React.Component <IMediaOptions> {
 					controls={this.props.controls}
 					loop={this.props.loop}
 					muted={this.props.muted}
+					ref={audio => (this.element = audio)}
 				>
 					<source src={this.props.source} type='audio/mpeg' />
 				</audio>
 			)
+
+		} else {
+
+			throw new Error('Component is neither Audio nor Video')
+
 		}
 
 	}
