c1af6a40bc77d0a65df29f258b17dc27edad2f92
itzezc <chirub@foretag.co>
2019-10-26 17:58:00 +0100

    [Component] Section responsive

--- source/components/Section/index.jsx
+++ source/components/Section/index.jsx
@@ -3,13 +3,14 @@
 import * as React from 'react'
 import styled from 'styled-components'
 
+import { device } from '../../mediaQuery'
 
 type IProps = {
     
     background?: string,
 
     children?: React.Node
-    
+
 }
 
 const Section = (props: IProps) => {
@@ -17,8 +18,15 @@ const Section = (props: IProps) => {
     const Section = styled.section`
         background-color: ${props.background || '#1e375a'};
         width: 100%;
-        padding: 80px 0;
         color: white;
+
+        @media ${device.tablet} {
+            padding: 50px 0;
+        }
+
+        @media ${device.laptop} {
+            padding: 80px 0;
+        }
     `
 
     return(
