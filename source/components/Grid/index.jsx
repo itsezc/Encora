83874ee44f61f8def641a9cc1c0d534048ccbf45
itzezc <chirub@foretag.co>
2019-12-22 13:47:32 +0000

    Basic grid

---
+++ source/components/Grid/index.jsx
@@ -0,0 +1,18 @@
+// @flow
+
+import * as React from 'react'
+import styled from 'styled-components'
+
+
+/**
+	* A Grid is a layout component
+	*
+	* @version 1.0.0
+	* @author [Chiru B](https://github.com/itsezc)
+*/
+const Grid = styled.div`
+	display: flex;
+`
+
+/** @component */
+export default Grid
