209094a169754245607f0027dc8fc1265952e239
itzezc <chirub@foretag.co>
2020-01-22 03:33:40 +0000

    [server] Cruddl create new project

--- server/cruddl/index.js
+++ server/cruddl/index.js
@@ -1,4 +1,4 @@
-import { ArangoDBAdapter } from 'cruddl'
+import { ArangoDBAdapter, Project } from 'cruddl'
 
 export const connection = (
 	name: string,
@@ -13,3 +13,37 @@ export const connection = (
 	user,
 	password: pass 
 })
+
+export const newProject = (connection, schema) => {
+	
+	const project = new Project({
+		sources: [
+			{
+				name: 'schema.graphql',
+				body: schema
+			},
+			{
+				name: 'permission-profiles.json',
+				body: JSON.stringify({
+					permissionProfiles: {
+						default: {
+							permissions: [
+								{
+									roles: ['users'],
+									access: 'readWrite'
+								}
+							]
+						}
+					}
+				})
+			}
+		],
+		getExecutionOptions: ({ context }) => ({ authRoles: ['users'] }),
+		getOperationIdentifier: ({ context }) => context
+	})
+
+	const schema = project.createSchema(connection)
+	connection.updateSchema(project.getModel())
+
+	return schema
+}
